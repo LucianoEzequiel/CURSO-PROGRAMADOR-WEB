bb2b5277ac0c102434f3663d6027afcb6b143889
LucianoEzequiel <lucianoezequiel76@gmail.com>
2021-09-20 16:34:58 -0300

    guardar cambios

--- Modulo N° 3/Unidad N° 1/Proyecto/routes/index.js
+++ Modulo N° 3/Unidad N° 1/Proyecto/routes/index.js
@@ -2,37 +2,36 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
-/* GET home page. */
+/* GET home page.*/
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/', async (req, res, next) => {
-
+  
   var Nombres = req.body.Nombres;
   var Apellido = req.body.Apellido;
   var Email = req.body.Email;
   var Telefono = req.body.Teléfono;
   var Mensaje = req.body.Mensaje;
 
-  //console.log(req.body);
+  console.log(req.body);
 
   var obj = {
     to: 'lucianoezequiel76@gmail.com',
-    subject: 'Contacto desde Sabores',
-    html: nombres + " " + apellido + " se contacto desde Sabores y necesita mas informacion a este correo: " + email + ". <br> Ademas, hizo el siguiente comentario: " + mensaje + ". <br> Su telefono es " + telefono
-  }
+    subject: 'Contacto desde',
+    html: Nombres + " " + Apellido + " se contacto y necesita mas informacion a este correo: " + Email + ". <br> Ademas, hizo el siguiente comentario: " + Mensaje + ". <br> Su telefono es " + Telefono,
+  };
 
-  var transporter = nodemailer.createTransport({
+  var transport = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
       user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    }
-  })
+    },
+  });
 
-  var info = await transporter.sendMail(obj);
+  var info = await transport.sendMail(obj);
 
   res.render('index', {
     message: 'Mensaje enviado',
