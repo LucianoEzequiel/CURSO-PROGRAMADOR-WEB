303d59b51845998120399f8e633820560b860a48
LucianoEzequiel <lucianoezequiel76@gmail.com>
2021-09-26 23:58:58 -0300

    guardado nuevo

--- Modulo N° 3/Unidad N° 1/Proyecto/routes/index.js
+++ Modulo N° 3/Unidad N° 1/Proyecto/routes/index.js
@@ -7,16 +7,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//router.post('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
-//var nombre = req.body.Nombre;
-//var apellido = req.body.Apellido;
-//var email = req.body.Email;
-//var telefono = req.body.Teléfono;
-//var mensaje = req.body.Mensaje;
+var Nombres = req.body.Nombre;
+var Apellido = req.body.Apellido;
+var Email = req.body.Email;
+var Telefono = req.body.Telefono;
+var Mensaje = req.body.Mensaje;
 
-//console.log(req.body);
+console.log(req.body);
 
+var obj = {
+  to: 'lucianoezequiel76@gmail.com',
+  subjet: 'contacto desde la web',
+  html: Nombres+ ' ' + Apellido + ' se contacto a traves y necesita mas informacion al correo: ' + Email + '. <br> Ademas, hizo el siguiente comentario: ' + Mensaje + '.  <br> Su telefono es ' + Telefono
+}
 
+var transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+});
+
+var info = await transporter.sendMail(obj);
+
+res.render('index', { message: 'Mensaje enviado correctamente',});
+
+});
 
 module.exports = router;
